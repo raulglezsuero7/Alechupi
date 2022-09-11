6d02bd6294bf090601dc6714de89ae99de36f63d
raulglezsuero7 <108340802+raulglezsuero7@users.noreply.github.com>
2022-09-11 16:42:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 var audio = document.getElementById("cancion");
 audio.volume = 0.1;
 
+var video = document.getElementById('video');
+var btnAbrirVideo = document.getElementById('boom'),
+        videoOverlay = document.getElementById('video-overlay'),
+        videoPopup = document.getElementById('video-popup'),
+        btnCerrarVideo = document.getElementById('btn-cerrar-video');
+
+        btnAbrirVideo.addEventListener('click', function()
+        {
+            videoOverlay.classList.add('active');
+            videoPopup.classList.add('active');
+        });
+
+        btnCerrarVideo.addEventListener('click', function()
+        {
+            videoOverlay.classList.remove('active');
+            videoPopup.classList.remove('active');
+            video.pause();
+        });
